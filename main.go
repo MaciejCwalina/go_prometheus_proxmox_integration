ef507c9fa3f3907df997ef972607693bd2cd39ab
MaciejCwalina <150276683+MaciejCwalina@users.noreply.github.com>
2024-08-02 22:53:58 +0200

    Added: Cpu info scrapping Disk io scraping

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -11,12 +12,40 @@ import (
 )
 
 var (
-	guage = promauto.NewGaugeVec(
+	proxmoxStatusInfoGuage = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "proxmox_status_info",
 			Help: "Gets information about proxmox",
 		},
+
 		[]string{"name", "id", "status"},
+	)
+
+	proxmoxVMCpuInfo = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "proxmox_vm_cpu_info",
+			Help: "Gets the information about the CPU usage in vms",
+		},
+
+		[]string{"name", "cpu_usage", "max_cores"},
+	)
+
+	proxmoxDiskRead = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "proxmox_disk_read",
+			Help: "Gets the disk read of vms",
+		},
+
+		[]string{"name"},
+	)
+
+	proxmoxDiskWrite = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "proxmox_disk_write",
+			Help: "Gets the disk write of vms",
+		},
+
+		[]string{"name"},
 	)
 )
 
@@ -28,7 +57,8 @@ func RecordMetrics(systemManager *SystemManager) {
 				log.Fatal("Failed to Get Infrastructure Information", err.Error())
 			}
 
-			guage.Reset()
+			proxmoxStatusInfoGuage.Reset()
+			proxmoxVMCpuInfo.Reset()
 			systemManager.PopulateSlices(infraData)
 			for _, vm := range systemManager.vms {
 				var isRunning float64
@@ -38,7 +68,10 @@ func RecordMetrics(systemManager *SystemManager) {
 					isRunning = 0
 				}
 
-				guage.WithLabelValues(vm.Name, vm.ID, vm.Status).Set(isRunning)
+				proxmoxStatusInfoGuage.WithLabelValues(vm.Name, vm.ID, vm.Status).Set(isRunning)
+				proxmoxVMCpuInfo.WithLabelValues(vm.Name, strconv.FormatFloat(vm.CPU, 'E', -1, 64), strconv.Itoa(vm.MaxCPU)).Set(vm.CPU)
+				proxmoxDiskRead.WithLabelValues(vm.Name).Set(float64(vm.DiskRead))
+				proxmoxDiskWrite.WithLabelValues(vm.Name).Set(float64(vm.DiskWrite))
 			}
 
 			time.Sleep(5 * time.Second)
